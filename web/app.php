674408b19638a470ca3cdd20a9b82cb7e40cfc98
woutersioen <wouter.sioen@gmail.com>
2015-01-19 10:06:35 +0100

    Use only one front controller.

--- web/app.php
+++ web/app.php
@@ -2,22 +2,30 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$env = getenv('SYMFONY_ENV') ? : 'prod';
+$debug = getenv('SYMFONY_DEBUG') === '1';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
-*/
+if ($env != 'dev') {
+    $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
+} else {
+    $loader = require_once __DIR__ . '/../app/autoload.php';
+}
+
+if ($debug) {
+    Debug::enable();
+}
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+$kernel = new AppKernel($env, $debug);
+
+if ($env != 'dev') {
+    $kernel->loadClassCache();
+}
+
 //$kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
