1d963e1b377a633a666361575bf7383d72e03a32
Tijs Verkoyen <tijs@verkoyen.eu>
2015-02-28 16:47:14 +0100

    Better comparison

--- src/SumoCoders/FrameworkCoreBundle/Menu/MenuBuilder.php
+++ src/SumoCoders/FrameworkCoreBundle/Menu/MenuBuilder.php
@@ -90,7 +90,7 @@ class MenuBuilder
 
             $orderNumber = $menuItem->getExtra('orderNumber');
 
-            if ($orderNumber != null) {
+            if (null !== $orderNumber) {
                 if (!isset($menuOrderArray[$orderNumber])) {
                     $menuOrderArray[$orderNumber] = $menuItem->getName();
                 } else {
