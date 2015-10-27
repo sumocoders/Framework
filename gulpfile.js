698fc2b0fa2e5c930083d070d7da63d7d8f6b93d
woutersioen <wouter.sioen@gmail.com>
2015-10-27 15:52:19 +0100

    Only start concatting files starting from an env check.

--- gulpfile.js
+++ gulpfile.js
@@ -101,26 +101,40 @@ gulp.task('js', function() {
 gulp.task('js:concat', function() {
   var cmd = 'cat src/SumoCoders/FrameworkCoreBundle/Resources/views/base.html.twig | grep script -C1 | grep asset -C1';
 
-  var grouped = [];
   var inputFiles = [];
-  var fullGroup = false;
+  var grouped = [];
+  var fullGroup = started = false;
+
   exec(cmd, function(error, stdout, stderr) {
     // group which files should be concatted to which files
     var lines = stdout.split('\n');
     lines.forEach(function(line) {
+      // check if an environment check starts here
+      if (line.indexOf('app.environment') > -1) {
+        started = true;
+        return;
+      }
+
+      if (!started) {
+        return;
+      }
+
+      // find the url and add it as destination or source script file
       if (line.indexOf('asset(\'') > -1) {
-        // strip the asset from it
         var url = /asset\('([^']+)'\)/g.exec(line)[1];
         if (fullGroup) {
           // we already have collected the dev files. Fetch the production file
           grouped[url] = inputFiles;
           inputFiles = [];
-          fullGroup = false;
-        } else {
-          // add a file
-          inputFiles.push('./web' + url);
+          fullGroup = started = false;
+          return;
         }
+
+        // add a file
+        inputFiles.push('./web' + url);
+        return;
       }
+
       // if we encounter an else statement, we'll get the prod file next
       if (line.indexOf('else') > -1) {
         fullGroup = true;
