39f4f05135c13c1f3d0f0dc8d500d1705fecb996
Tijs Verkoyen <tijs@verkoyen.eu>
2015-11-12 17:25:35 +0100

    Added defaults
    Because we don't like typing we made sure we can configure some defaults:
    
    * default sender
    * default to
    * default reply-to

--- src/SumoCoders/FrameworkCoreBundle/Mail/MessageFactory.php
+++ src/SumoCoders/FrameworkCoreBundle/Mail/MessageFactory.php
@@ -5,11 +5,84 @@ namespace SumoCoders\FrameworkCoreBundle\Mail;
 class MessageFactory
 {
     /**
+     * @var array
+     */
+    protected $sender = array();
+
+    /**
+     * @var array
+     */
+    protected $replyTo = array();
+
+    /**
+     * @var array
+     */
+    protected $to = array();
+
+    /**
+     * Set the default sender
+     *
+     * @param string      $email
+     * @param string|null $name
+     */
+    public function setDefaultSender($email, $name = null)
+    {
+        if ($name !== null) {
+            $this->sender = array($email => $name);
+        } else {
+            $this->sender = $email;
+        }
+    }
+
+    /**
+     * Set the default reply-to
+     *
+     * @param string      $email
+     * @param string|null $name
+     */
+    public function setDefaultReplyTo($email, $name = null)
+    {
+        if ($name !== null) {
+            $this->replyTo = array($email => $name);
+        } else {
+            $this->replyTo = $email;
+        }
+    }
+
+    /**
+     * Set the default to
+     *
+     * @param string      $email
+     * @param string|null $name
+     */
+    public function setDefaultTo($email, $name = null)
+    {
+        if ($name !== null) {
+            $this->to = array($email => $name);
+        } else {
+            $this->to = $email;
+        }
+    }
+
+    /**
      * @return Message
      */
-    public static function createMessage()
+    public function createMessage()
     {
         $message = new Message();
+
+        if (!empty($this->sender)) {
+            $message->setFrom($this->sender);
+        }
+
+        if (!empty($this->replyTo)) {
+            $message->setReplyTo($this->replyTo);
+        }
+
+        if (!empty($this->to)) {
+            $message->setTo($this->to);
+        }
+
         return $message;
     }
 }
