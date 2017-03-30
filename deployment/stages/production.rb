5ecfcb3196a4e5e3ad10dc4ea723d22dafd447e6
Tijs Verkoyen <tijs@verkoyen.eu>
2017-03-30 14:29:38 +0200

    Removed old Capistrano folders

--- deployment/stages/production.rb
+++
@@ -1,9 +0,0 @@
-set :host,          "xxx" # eg. web03.sumocoders.be
-set :user,          "xxx" # eg. sumocoders
-set :domain,        "xxx" # eg. app.sumoapp.be"
-set :deploy_to,     "/home/#{user}/apps/#{client}/#{project}"
-set :document_root, "/home/#{user}/#{domain}"
-set :branch,        "master"
-
-### DO NOT EDIT BELOW ###
-server "#{host}", :app, :web, :db, :primary => true
