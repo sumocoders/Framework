b7b1978e069b76fec24d3575384d965a90cf7a15
Tijs Verkoyen <tijs@verkoyen.eu>
2017-03-30 17:13:40 +0200

    Set linked-stuff

--- app/config/capistrano/deploy.rb
+++ app/config/capistrano/deploy.rb
@@ -4,11 +4,8 @@ lock "3.8.0"
 set :application, "framework"
 set :repo_url, "git@github.com:sumocoders/Framework.git"
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_files, "app/config/parameters.yml"
+append :linked_dirs, "app/logs"
 
 set :symfony_console_path, "app/console"
 
