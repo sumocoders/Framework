643d9c673d41ddba94edda13d9f43dc0b7f61939
Tijs Verkoyen <tijs@verkoyen.eu>
2017-03-30 18:06:56 +0200

    Use known variables

--- app/config/capistrano/deploy.rb
+++ app/config/capistrano/deploy.rb
@@ -1,12 +1,14 @@
-# config valid only for current version of Capistrano
-lock "3.8.0"
-
-set :application, "framework"
+set :client,  "client"
+set :project, "framework"
 set :repo_url, "git@github.com:sumocoders/Framework.git"
+
+### DO NOT EDIT BELOW ###
+lock "3.8.0"
 
 append :linked_files, "app/config/parameters.yml"
 append :linked_dirs, "app/logs"
 
+set :application, "#{fetch :project}"
 set :symfony_console_path, "app/console"
 
 namespace :deploy do
--- app/config/capistrano/stages/staging.rb
+++ app/config/capistrano/stages/staging.rb
@@ -1,33 +1,17 @@
+set :branch, "master"
+
+### DO NOT EDIT BELOW ###
+
+set :deploy_to, "/home/sites/apps/#{fetch :client}/#{fetch :project}"
 server "dev02.sumocoders.eu", user: "sites", roles: %w{app db web}
 
-set :deploy_to, "/home/sites/apps/client/framework"
+set :keep_releases,  2
 
 SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
 
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+### OLD ###
+#set :domain,        "#{project}.#{client}.php71.sumocoders.eu"
+#set :document_root, "/home/#{user}/php71/#{client}/#{project}"
 #
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+#before 'deploy:setup', 'sumo:setup:client_folder'
+#before 'deploy:setup', 'sumo:db:create'
