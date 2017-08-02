aaaab8ee9e2ba9ea612b8d6c4167e413319e4d4b
Tijs Verkoyen <tijs@verkoyen.eu>
2017-08-02 09:44:06 +0200

    Fixed some styling

--- app/config/capistrano/deploy.rb
+++ app/config/capistrano/deploy.rb
@@ -1,41 +1,42 @@
-set :client,  ""
-set :project, ""
-set :repo_url, ""
-set :production_url, ""
+set :client,  ''
+set :project, ''
+set :repo_url, ''
+set :production_url, ''
 
 ### DO NOT EDIT BELOW ###
-lock "3.8.0"
-
-append :linked_files, "app/config/parameters.yml"
-append :linked_dirs, "app/logs"
-
 set :application, "#{fetch :project}"
-set :symfony_console_path, "app/console"
 
 set :deploytag_utc, false
-set :deploytag_time_format, "%Y%m%d-%H%M%S"
+set :deploytag_time_format, '%Y%m%d-%H%M%S'
+
+set :files_dir, %w(web/files files)
+
+append :linked_files, 'app/config/parameters.yml'
+append :linked_dirs, 'app/logs'
+
+set :symfony_console_path, 'app/console'
 
 namespace :deploy do
-  after :check, "framework:symlink:document_root"
+  after :check, 'framework:symlink:document_root'
 
-  after :starting, "composer:install_executable"
-  after :starting, "opcache:phpfpm:install_executable"
+  after :starting, 'composer:install_executable'
+  after :starting, 'opcache:phpfpm:install_executable'
 
-  before :publishing, "assets:upload"
+  before :publishing, 'assets:upload'
 
-  after :published, "opcache:phpfpm:reset"
-  after :published, "migrations:migrate"
+  after :published, 'opcache:phpfpm:reset'
+  after :published, 'migrations:migrate'
 
-  after :finished, "sumo:notifications:deploy"
+  after :finished, 'sumo:notifications:deploy'
 
-  after :failed, "maintenance:disable"
+  after :failed, 'maintenance:disable'
 end
 
 namespace :assets do
-  after :upload, "assets:update_assets_version"
+  after :upload, 'assets:update_assets_version'
 end
 
 namespace :migrations do
-  before :migrate, "maintenance:enable"
-  after :migrate, "maintenance:disable"
+  before :migrate, 'maintenance:enable'
+  after :migrate, 'maintenance:disable'
 end
